f4186f1a2853cb41f0d7a7f1eefafbd665533d13
lisacopeland <lisacope@msn.com>
2019-08-02 11:47:23 -0700

    Changed table

--- README.md
+++ README.md
@@ -2,48 +2,28 @@
 
 ## Create Database
 
-Make sure MySQL is installed and running on the system. Create a `api` table and run the query:
+Make sure MySQL is installed and running on the system. Create a `orders` table and run the query:
 
 ```sql
-CREATE TABLE `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) DEFAULT '',
-  `username` varchar(50) DEFAULT '',
-  `email` varchar(50) DEFAULT '',
+CREATE TABLE buy_order (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `name` char(255),
+  `max_bid_price` decimal(10,2),
+  `data_package_type` enum('Device Location', 'Device Behavior', 'ID Mapping'),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 ```
 
 ## Installation
 
 Clone, install, and start up the app.
 
-```bash
-git clone https://github.com/taniarascia/node-api.git
-cd node-api
+```
+git clone https://github.com/lisacopeland/mysqlserver
+cd mysqlserver
 npm install
 node app.js
 ```
 
-Navigate to `http://localhost:3002`.
-
-## HTTP Requests
-
-Use `node post.js` to send a `POST` request through, or use `curl` to do all HTTP CRUD requests.
-
-### GET
-
-- `curl http://localhost:3002/users`
-- `curl http://localhost:3002/users/1`
-
-### POST 
-
-- `curl --data "name=Tania&email=tania@example.com&username=floppydiskette" http://localhost:3002/users`
-
-### PUT
-
-- `curl -X PUT -d "name=Tania" -d "email=tania@example.com" -d "username=taniarascia" http://localhost:3002/users/1`
-
-### DELETE
-
-- `curl -X "DELETE" http://localhost:3002/users/1`
+Navigate to `http://localhost:3002` to see welcome message and verify that server
+is running
--- routes/routes.js
+++ routes/routes.js
@@ -5,13 +5,13 @@ const pool = require('../data/config');
 const router = app => {
     // Display welcome message on the root
     app.get('/', (request, response) => {
-        response.send({message: 'Welcome to the Node.js Express REST API!'});
+        response.send({message: 'Welcome to the Lisa Copeland buy_order REST API Server!'});
     });
 
     // Display all cars
-    app.get('/cars', (request, response) => {
+    app.get('/buyorders', (request, response) => {
         console.log('GET request display all cars');
-        pool.query('SELECT * FROM car', (error, result) => {
+        pool.query('SELECT * FROM buy_order', (error, result) => {
             console.log('result from query : ' + JSON.stringify(result));
             if (error) {
                 console.log('error sending error message ' + error.message);
@@ -27,10 +27,10 @@ const router = app => {
     });
 
     // Display a single car by ID
-    app.get('/cars/:id', (request, response) => {
+    app.get('/buyorder/:id', (request, response) => {
         const id = request.params.id;
         console.log('GET request for id : ' + id);        
-        pool.query('SELECT * FROM car WHERE id = ?', id, (error, result) => {
+        pool.query('SELECT * FROM buy_order WHERE id = ?', id, (error, result) => {
             console.log('result from query : ' + JSON.stringify(result));
             if (error) {
                 // throw error;
@@ -49,7 +49,7 @@ const router = app => {
     });
 
     // Add a new car
-    app.post('/cars', (request, response) => {
+    app.post('/buyorder', (request, response) => {
         console.log('POST request, body : ' + JSON.stringify(request.body));
         pool.query('INSERT INTO car SET ?', request.body, (error, result) => {
             console.log('result from query : ' + JSON.stringify(result));        
@@ -69,10 +69,10 @@ const router = app => {
     });
 
     // Update an existing car
-    app.put('/cars/:id', (request, response) => {
+    app.put('/buyorder/:id', (request, response) => {
         const id = request.params.id;
         console.log('PUT request for id ' + id);
-        pool.query('UPDATE car SET ? WHERE id = ?', [request.body, id], (error, result) => {
+        pool.query('UPDATE buy_order SET ? WHERE id = ?', [request.body, id], (error, result) => {
             console.log('result from query : ' + JSON.stringify(result));
             if (error) {
                 response.status(400).send({ error: "error" });
@@ -86,10 +86,10 @@ const router = app => {
     });
 
     // Delete a car
-    app.delete('/cars/:id', (request, response) => {
+    app.delete('/buyorder/:id', (request, response) => {
         const id = request.params.id;
         console.log('DELETE request for id ' + id);
-        pool.query('DELETE FROM car WHERE id = ?', id, (error, result) => {
+        pool.query('DELETE FROM buy_order WHERE id = ?', id, (error, result) => {
             console.log('result from query : ' + JSON.stringify(result));
             if (error) {
                 response.status(400).send({ error: error.message });
